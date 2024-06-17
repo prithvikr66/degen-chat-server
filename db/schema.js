5c7756c0cebeb0556206dc2b9af9760dba6a1a80
prithvikr66 <pkunofficial66@gmail.com>
2024-06-17 23:50:08 +0530

    fixed cors error (hopefully :|)

--- db/schema.js
+++ db/schema.js
@@ -1,11 +1,17 @@
 const mongoose = require("mongoose");
 
+const userSchema = new mongoose.Schema({
+  username: String,
+  walletAddress: String,
+  profilePic: String,
+});
 const messageSchema = new mongoose.Schema({
   username: String,
   message: String,
   timestamp: { type: Date, default: Date.now },
 });
 
+const Users = mongoose.model("Users", userSchema);
 const Message = mongoose.model("Message", messageSchema);
 
-module.exports = { Message };
+module.exports = { Message, Users };
--- index.js
+++ index.js
@@ -1,27 +1,81 @@
 // server.js
 const express = require("express");
 const http = require("http");
-const {Server} = require("socket.io");
-const mongoose = require("mongoose");
+const { Server } = require("socket.io");
 const connectDB = require("./db/connect");
+const { Users, Message } = require("./db/schema");
 const cors = require("cors");
 const app = express();
 const server = http.createServer(app);
-const io = new Server({
-  cors: "*",
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
 });
-io.attach(server);
-
 
 connectDB();
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+app.use(express.json());
+app.options('*',cors());
+app.get("/", (req, res) => {
+  res.json({ success: true });
+});
+var allowCrossDomain = function(req,res,next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();  
+}
+app.use(allowCrossDomain);
+
+
+
+app.post("/api/profile", async (req, res) => {
+  try {
+    const { username, walletAddress } = req.body;
+    const profilePic = "";
+    const existingUser = await Users.findOne({ username });
+
+    if (existingUser) {
+      return res.status(400).json({
+        message: "Username Already Taken !",
+      });
+    }
+    const newUser = new Users({ username, walletAddress, profilePic });
+    await newUser.save();
+    res.status(201).json({ message: "Username created successfully" });
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+});
+
+
+app.get("/api/initialMessages", async (req, res) => {
+  try {
+    const messages = await Message.find().sort({ timestamp: -1 }).limit(50) .select('-timestamp ');
+    messages.reverse();
+    return res.status(200).json(messages);
+  } catch (err) {
+    console.log(err.message);
+  }
+});
 
 io.on("connection", (socket) => {
   console.log("A user connected");
 
-  socket.on("sendMessage", (msg) => {
-    io.emit("newMessage", msg);
+  socket.on("sendMessage", async (msg) => {
     console.log(msg);
+    io.emit("newMessage", msg);
+    const message = new Message(msg);
+    await message.save();
   });
 
   socket.on("disconnect", () => {

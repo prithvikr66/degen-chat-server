3410aa65c86e1366c66fa84245b2e7b17ae93be9
reetbatra <reetbatra25@gmail.com>
2024-07-22 23:35:02 +0530

    deployment

--- db/connect.js
+++ db/connect.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://pkunofficial66:WpeYUJqtkBce7Rxv@cluster0.baim4sk.mongodb.net/"
+      "mongodb+srv://pokeharsh:1234@cluster0.3xo6scr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     );
   } catch (err) {
     console.log(err.message);
